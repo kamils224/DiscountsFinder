9714dd3cb95a24bde77484c394e8e5af481b0021
kamils224 <kamil.sagalara@gmail.com>
2021-05-10 14:35:06 +0200

    Mongodb initial configuration

--- discounts_finder/config.py
+++ discounts_finder/config.py
@@ -4,7 +4,6 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://app.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     CELERY_BROKER_URL = 'redis://localhost:6379/0'
     CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+    MONGO_URI = 'mongodb://mongodb/discounts_finder_db'
--- discounts_finder/resources.py
+++ discounts_finder/resources.py
@@ -1,14 +1,2 @@
 from flask_restful import Resource
 
-from tasks.tasks import test
-from schema import ProductSchema
-from models import Product
-
-products_schema = ProductSchema(many=True)
-
-
-class Products(Resource):
-    def get(self):
-        test.delay()
-        products = Product.query.all()
-        return products_schema.dump(products)
