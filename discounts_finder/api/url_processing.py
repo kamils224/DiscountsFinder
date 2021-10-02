0337306adfd551ab04aa5f1f318e0ae1548504a3
kamils224 <kamil.sagalara@gmail.com>
2021-10-02 17:02:26 +0200

    Fix problem with processing task in celery

--- discounts_finder/api/url_processing.py
+++ discounts_finder/api/url_processing.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from flask_restful import reqparse
 
+from discounts_finder.celery_queue.tasks import process_products_url
 from discounts_finder.parsers.network_utils import get_products_from_url
 from discounts_finder.api.serializers.web_product_serializer import WebProductSerializer
 
@@ -12,7 +13,19 @@ class ProductsUrlProcessing(Resource):
         parser.add_argument("url", type=str)
         args = parser.parse_args()
         url = args["url"]
-        #products = get_products_from_url(url)
-        #serializer = WebProductSerializer()
-        #return {"products": [serializer.dump(product) for product in products]}
-        return {"response": "url"}
+        products = get_products_from_url(url)
+        serializer = WebProductSerializer()
+
+        return {"products": [serializer.dump(product) for product in products]}
+
+
+class ProductsUrlProcessingQueue(Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("url", type=str)
+        args = parser.parse_args()
+        url = args["url"]
+        result = process_products_url.delay(url)
+
+        return {"task_id": result.id, "status": "processing"}
