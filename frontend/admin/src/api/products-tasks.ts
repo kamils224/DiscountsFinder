c5f5d6a097f25e348a8be4aefac5dd094f584454
kamils224 <kamil.sagalara@gmail.com>
2021-11-07 17:42:28 +0100

    Refactor products tasks dto and display for it

--- frontend/admin/src/api/products-tasks.ts
+++ frontend/admin/src/api/products-tasks.ts
@@ -2,15 +2,14 @@ import axiosInstance from "../axios-config/axios.instance";
 
 export class ProductsTasksDto {
     constructor(public id: number,
-                public created: string,
+                public timestamp: number,
                 public status: string,
                 public pageUrl: string,
                 public count: number) {
     }
 
     static fromJson(json: Record<string, any>) {
-        const created = new Date(json.timestamp * 1000);
-        return new ProductsTasksDto(json._id, `${created.toLocaleDateString()} ${created.toLocaleTimeString()}`,
+        return new ProductsTasksDto(json._id, json.timestamp,
             json.status, json.page_url, json.count);
     }
 }
--- frontend/admin/src/views/tasks-list.tsx
+++ frontend/admin/src/views/tasks-list.tsx
@@ -22,7 +22,7 @@ const columns = [
         title: "Url",
         dataIndex: "pageUrl",
         key: 'pageUrl',
-        render: (_: string, record: ProductsTasksDto) => {
+        render: (_: string, record: Record<string, any>) => {
             return (
                 <a href={record.pageUrl}>{record.pageUrl}</a>
             )
@@ -42,17 +42,29 @@ const columns = [
     },
 ];
 
+const productsTasksDisplay = (productsTasks: ProductsTasksDto): Record<string, any> => {
+    const created = new Date(productsTasks.timestamp * 1000);
+    return {
+        id: productsTasks.id,
+        created: `${created.toLocaleDateString()} | ${created.toLocaleTimeString()}`,
+        status: productsTasks.status,
+        pageUrl: productsTasks.pageUrl,
+        count: productsTasks.count
+    }
+}
+
 export default function TasksList() {
-    const [tasks, setTasks] = useState<Array<ProductsTasksDto>>([]);
+    const [tasks, setTasks] = useState<Array<Record<string, any>>>([]);
 
     async function fetchTasks(): Promise<Array<ProductsTasksDto>> {
         const api = new ProductsTasksApi();
         return await api.getProductsTasks();
 
     }
+
     useEffect(() => {
         fetchTasks().then(result => {
-            setTasks(result)
+            setTasks(result.map((item) => productsTasksDisplay(item)))
         });
     }, [])
 
