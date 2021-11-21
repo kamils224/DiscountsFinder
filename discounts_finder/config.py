ed2b566182a3988980365fdb1e87d14a7eaea601
kamils224 <kamil.sagalara@gmail.com>
2021-11-21 22:02:44 +0100

    Change docker-compose and add template minikube deployment

--- discounts_finder/config.py
+++ discounts_finder/config.py
@@ -8,6 +8,6 @@ class Config:
     CELERY_BROKER_URL = "redis://localhost:6379/0"
     imports = ("discounts_finder.celery_worker.tasks",)
     MONGO_URI = (
-        f"mongodb://{os.environ['MONGO_INITDB_ROOT_USERNAME']}:{os.environ['MONGO_INITDB_ROOT_PASSWORD']}"
-        f"@mongodb/{os.environ['MONGO_INITDB_DATABASE']}?authSource=admin"
+        f"mongodb://{os.environ.get('MONGO_INITDB_ROOT_USERNAME')}:{os.environ.get('MONGO_INITDB_ROOT_PASSWORD')}"
+        f"@mongodb/{os.environ.get('MONGO_INITDB_DATABASE')}?authSource={os.environ.get('MONGO_INITDB_AUTH_SOURCE')}"
     )
